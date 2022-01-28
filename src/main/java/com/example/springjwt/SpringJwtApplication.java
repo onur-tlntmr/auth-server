385ab35917c2b3879fedcc05a5e8ede6b581da7a
Onur Talantimur <onurtalantimur@gmail.com>
2022-01-28 21:20:08 +0300

    UserController and PasswordEncoder Bean  has added.

--- src/main/java/com/example/springjwt/SpringJwtApplication.java
+++ src/main/java/com/example/springjwt/SpringJwtApplication.java
@@ -2,6 +2,9 @@ package com.example.springjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringJwtApplication {
@@ -9,5 +12,11 @@ public class SpringJwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJwtApplication.class, args);
 	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 }
---
+++ src/main/java/com/example/springjwt/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.springjwt.controller;
+
+import com.example.springjwt.model.Role;
+import com.example.springjwt.model.User;
+import com.example.springjwt.service.UserService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
+}
+
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+
+        return ResponseEntity.ok().build();
+    }
+
+}
