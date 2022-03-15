8744d9902edf5cb74199316f39eaf73ea0d0d198
Onur Talantimur <onurtalantimur@gmail.com>
2022-03-15 20:43:16 +0300

    RoleController has been separated and request mapping refactored.

--- src/main/java/com/example/springjwt/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/example/springjwt/filter/CustomAuthorizationFilter.java
@@ -23,7 +23,8 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login"))
+
+        if (request.getServletPath().equals("/login"))
             filterChain.doFilter(request, response);
 
         else {
