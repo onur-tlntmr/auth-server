b8be817c247f5ab7c0e113ce1ca0fc35fcd8ba05
Onur Talantimur <onurtalantimur@gmail.com>
2022-04-10 20:01:16 +0300

    Access token expiry time set 5 minutes.

--- src/main/java/com/example/springjwt/util/SecurityConstant.java
+++ src/main/java/com/example/springjwt/util/SecurityConstant.java
@@ -7,8 +7,8 @@ public class SecurityConstant {
 
     public static final String ISSUER = "example.com";
 
-    // 5 hour for development
-    public static final int EXPIRES_ACCESS_TOKEN = 5 * 60 * 60 * 1000;
+    // 5 minutes
+    public static final int EXPIRES_ACCESS_TOKEN = 5 * 60 * 1000;
 
     // Expires date in months
     public static final int EXPIRES_MONTH_REFRESH_TOKEN = 6;
